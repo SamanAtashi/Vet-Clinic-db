9dee483988e8f4eb6474a03f1ff9316ce4d2d58e
SamanAtashi <milady00126@gmail.com>
2021-12-02 17:25:07 +0300

    Add data + queries

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-  id int,    
+  id BIGSERIAL,    
   name varchar(100),
   date_of_birth date,
   escape_attempts int,
