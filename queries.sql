a3d7d6dde526621914838f6240865ad05a837804
SamanAtashi <milady00126@gmail.com>
2021-12-03 14:33:38 +0300

    Add species --> ROLLBACK;

--- queries.sql
+++ queries.sql
@@ -1,20 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name LIKE '%mon';
+BEGIN;
 
+UPDATE animals 
+SET species = 'unspecified'
+WHERE species IS NULL;
 
-SELECT name FROM animals
-WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+ROLLBACK;
 
-SELECT name from animals WHERE neutered=true AND (escape_attempts<3);
-
-SELECT date_of_birth from animals WHERE name='Agumon' OR name='Pikachu';
-
-SELECT name ,escape_attempts from animals WHERE weight_kg>10.5;
-
-SELECT * from animals WHERE neutered=true ;
-
-SELECT * from animals WHERE name!='Gabumon';
-
-SELECT * FROM animals
-WHERE weight_kg>=10.4 AND weight_kg<=17.3 ;
+------------------------------
