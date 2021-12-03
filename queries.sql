33d67b8ef2470e708f23177a49e47b38a9f98a64
SamanAtashi <milady00126@gmail.com>
2021-12-03 14:41:46 +0300

    Add digimon/pokemon transaction -->commit;

--- queries.sql
+++ queries.sql
@@ -6,6 +6,30 @@ UPDATE animals
 SET species = 'unspecified'
 WHERE species IS NULL;
 
+
+SELECT * FROM animals;
+
 ROLLBACK;
 
+SELECT * FROM animals;
+
 ------------------------------
+
+BEGIN;
+
+SELECT * FROM animals WHERE name ILIKE '%mon';
+
+UPDATE animals 
+SET species = 'digimon'
+WHERE name ILIKE '%mon';
+
+UPDATE animals 
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+------------------------------
+
+
+
