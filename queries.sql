1e9e27b63095a4ab0101019910049fa89dfa2783
SamanAtashi <milady00126@gmail.com>
2021-12-03 15:19:26 +0300

    Add queries questions

--- queries.sql
+++ queries.sql
@@ -61,3 +61,25 @@ COMMIT;
 
 ------------------------------
 
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg)::numeric(10,2) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered FROM animals WHERE escape_attempts=(
+    SELECT MAX(escape_attempts)
+    FROM animals
+);
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT MAX(weight_kg) FROM animals;
+SELECT MIN(weight_kg) FROM animals;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT AVG(escape_attempts)::numeric(10,2) FROM animals 
+WHERE date_of_birth>'1990-01-01' AND date_of_birth<'2000-12-31';
