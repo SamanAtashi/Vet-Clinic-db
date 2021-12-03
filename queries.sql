b34d56d28511283c14f1134384f2beea94449c53
SamanAtashi <milady00126@gmail.com>
2021-12-03 15:03:16 +0300

    Add savepoint--> Commit;

--- queries.sql
+++ queries.sql
@@ -6,7 +6,6 @@ UPDATE animals
 SET species = 'unspecified'
 WHERE species IS NULL;
 
-
 SELECT * FROM animals;
 
 ROLLBACK;
@@ -38,3 +37,27 @@ DELETE FROM animals;
 SELECT * FROM animals;
 
 ROLLBACK;
+
+SELECT * FROM animals;
+
+------------------------------
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+
+SAVEPOINT temp;
+
+UPDATE animals
+SET weight_kg = weight_kg*(-1); 
+
+ROLLBACK TO SAVEPOINT temp;
+
+UPDATE animals
+SET weight_kg = weight_kg*(-1)
+WHERE weight_kg<0;
+
+COMMIT;
+
+------------------------------
+
