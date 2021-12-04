4af35946604a8b17203601795ced2d02b022eb35
SamanAtashi <milady00126@gmail.com>
2021-12-04 10:51:52 +0300

    Add species_id to animals

--- data.sql
+++ data.sql
@@ -10,3 +10,33 @@ INSERT INTO owners (full_name,age) VALUES ('Jodie Whittaker', 38);
 
 INSERT INTO species (name) VALUES ('Pokemon');
 INSERT INTO species (name) VALUES ('Digimon');
+
+
+INSERT INTO 
+animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Charmander' , date '2020-02-08' , 0 , false , -11);
+
+INSERT INTO 
+animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Plantmon' , date '2022-11-15' , 2 , true , -5.7);
+
+INSERT INTO 
+animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Squirtle' , date '1993-04-02' , 3 , false , -12.13);
+
+INSERT INTO 
+animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Angemon' , date '2005-06-12' , 1 , true , -45);
+
+INSERT INTO 
+animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Boarmon' , date '2005-06-07' , 7 , true , 20.4); 
+
+INSERT INTO 
+animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Blossom' , date '1998-10-13' , 3 , true , 17); 
+
+UPDATE animals SET species_id = 2;
+UPDATE animals SET species_id = 1 WHERE name ILIKE '%mon';
+
+
