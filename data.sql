6ff3cb2b887697b13e07380f49bad1cc9ca49333
SamanAtashi <milady00126@gmail.com>
2021-12-04 10:22:29 +0300

    Add data.sql -- owners insert

--- data.sql
+++ data.sql
@@ -1,16 +1,12 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Charmander' , date '2020-02-08' , 0 , false , -11);
+INSERT INTO owners (full_name,age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name,age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name,age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name,age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name,age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name,age) VALUES ('Jodie Whittaker', 38);
 
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Plantmon' , date '2022-11-15' , 2 , true , -5.7);
-
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Squirtle' , date '1993-04-02' , 3 , false , -12.13);
-
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Angemon' , date '2005-06-12' , 1 , true , -45);
-
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Boarmon' , date '2005-06-07' , 7 , true , 20.4); 
-
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Blossom' , date '1998-10-13' , 3 , true , 17); 
 
 
 
